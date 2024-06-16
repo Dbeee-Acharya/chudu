42a5f5bb64cfb61f0385b16ef5de5779704922e9
dbeee <dbeee.acharya@gmail.com>
2024-06-16 22:27:27 +0545

    Complete

--- script.js
+++ script.js
@@ -4,9 +4,15 @@ const landingNo = document.querySelector('.no');
 const body = document.querySelector('.body');
 const mainLandingSection = document.querySelector('.main-container');
 const answerNoSection = document.querySelector('.answer-no');
+const answerYesSection = document.querySelector('.answer-yes');
 
 landingNo.addEventListener('click', ()=>{
     mainLandingSection.classList.toggle('active');
-    body.classList.toggle('red')
+    body.classList.toggle('red');
     answerNoSection.classList.toggle('active');
+})
+
+landingYes.addEventListener('click', ()=>{
+    mainLandingSection.classList.toggle('active');
+    answerYesSection.classList.toggle('active');
 })
